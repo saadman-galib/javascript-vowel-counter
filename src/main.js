36f685e81f236e0e2981d0df9a07f2e51ff307ee
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-02-22 18:39:56 +0600

    Wrapping countVowels with a function

--- src/main.js
+++ src/main.js
@@ -5,38 +5,37 @@ var textLength = 0;
 var previousLength = 0;
 var previousText = "";
 
+function countVowels(text, textLength, keys) {
+    let vowelTotal = 0;
+    for (let i = 0; i < textLength; i++) {
+        if (keys.includes(text[i])) {
+            ++vowelTotal;
+        }
+    }
+
+    return vowelTotal;
+}
+
 textarea.addEventListener("keyup", (e) => {
     let keys = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     let text = textarea.value;
     textLength = text.length;
-    if (text.length > 0) {
+    if (text.length >= 0) {
         var lastChar = text[text.length - 1];
         var isVowel = keys.includes(lastChar);
 
         if (e.keyCode == 8) {
-            vowelTotal = 0;
-            for (let i = 0; i < textLength; i++) {
-                if (keys.includes(text[i])) {
-                    ++vowelTotal;
-                }
-            }
+            vowelTotal = vowelTotal = countVowels(text, textLength, keys);
         } else if (e.keyCode == 229) {
             if (textLength < previousLength) {
                 if (keys.includes(previousText[previousText.length - 1])) {
                     --vowelTotal;
                 }
             } else {
-                if (isVowel) {
-                    ++vowelTotal;
-                }
+                vowelTotal = countVowels(text, textLength, keys);
             }
         } else {
-            vowelTotal = 0;
-            for (let i = 0; i < textLength; i++) {
-                if (keys.includes(text[i])) {
-                    ++vowelTotal;
-                }
-            }
+            vowelTotal = countVowels(text, textLength, keys);
         }
     }
     previousLength = textLength;
